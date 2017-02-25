df706b3ee036cbfc1ed874dfbea70219f3f12354
Cphil31 <cecile_philippe@live.fr>
2017-02-25 23:27:13 +0100

    modif

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,38 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Home</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.8/semantic.min.css">
 </head>
 <body>
-<h1>hello world</h1>
+	<div class="ui grid">
+		<div class="six wide column"></div>
+		<div class="five wide column"></div>
+		<div class="four wide column"></div>	
+	</div>
+
+	
+	<div class="ui grid">
+		<div class="five wide column"></div>
+		<div class="six wide center aligned column">
+
+			<h1>CECILE PHILIPPE</h1>
+
+			<hr/>
+
+			<h1>Developpeur Web</h1>
+
+		</div>
+		<div class="five wide column"></div>	
+	</div>
+
+
+	<div class="ui grid">
+		<div class="six wide column"></div>
+		<div class="five wide column"></div>
+		<div class="four wide column"></div>	
+	</div>
+
+	
 	
 </body>
 </html>
