7ede4761765dcd2baa96c8c97a1671359f9f71c7
Cphil31 <cecile_philippe@live.fr>
2017-02-25 23:56:23 +0100

    ajout des pages du menu

---
+++ resources/views/loisirs.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.8/semantic.min.css">
+</head>
+<body>
+	<div class="ui grid">
+		<div class="six wide column"></div>
+		<div class="five wide column"></div>
+		<div class="four wide column"></div>	
+	</div>
+
+	
+	<div class="ui grid">
+		<div class="five wide column"></div>
+		<div class="six wide center aligned column">
+			<h1>CECILE PHILIPPE</h1>
+
+			<hr/>
+
+			<h1>Developpeur Web</h1>
+
+			<div class="ui five item menu">
+				<a class="item">Formations</a>
+				<a class="item">Compétences</a>
+				<a class="item">Expériences</a>
+				<a class="item">Loisirs</a>
+				<a class="item">Contact</a>
+			</div>
+
+
+
+		</div>
+		<div class="five wide column"></div>	
+	</div>
+
+	<div class="ui grid">
+		<div class="four wide column"></div>
+		<div class="four wide column"></div>
+		<div class="four wide column"></div>
+
+	</div>
+
+
+	
+	
+</body>
+</html>
