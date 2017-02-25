c32ec13765175e86bae1bf2fd58d7dc7e72d6d35
Cphil31 <cecile_philippe@live.fr>
2017-02-25 23:45:06 +0100

    npm install

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,23 +16,33 @@
 	<div class="ui grid">
 		<div class="five wide column"></div>
 		<div class="six wide center aligned column">
-
 			<h1>CECILE PHILIPPE</h1>
 
 			<hr/>
 
 			<h1>Developpeur Web</h1>
 
+			<div class="ui five item menu">
+				<a class="item">Formations</a>
+				<a class="item">Compétences</a>
+				<a class="item">Expériences</a>
+				<a class="item">Loisirs</a>
+				<a class="item">Contact</a>
+			</div>
+
+
+
 		</div>
 		<div class="five wide column"></div>	
 	</div>
 
+	<div class="ui grid">
+		<div class="four wide column"></div>
+		<div class="four wide column"></div>
+		<div class="four wide column"></div>
 
-	<div class="ui five item menu">
-		<a class="item">Editorials</a>
-		<a class="item">Reviews</a>
-		<a class="item">Upcoming Events</a>
 	</div>
+
 
 	
 	
